e75232033f6ab594a52ecc2626e17666955eb0e9
kozakevich13 <hanibalsmitti@gmail.com>
2023-02-09 18:36:47 +0200

    add btn to menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,13 @@
 import './Header.css';
 import Logo from '../logo/Logo'
+import Menu from '../menu/Menu';
 
 
 function Header() {
   return (
     <div className="header">
        <Logo/>
+       <Menu/>
     </div>
   );
 }
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,16 @@
+import './Menu.css';
+
+function Menu() {
+  return (
+    <div className="menu">
+        <a className='menu-link' href=''>Головна</a>
+        <a className='menu-link' href=''>Про нас</a>
+        <a className='menu-link' href=''>ОПТ</a>
+        <a className='menu-link' href=''>Сервіс</a>
+        <a className='menu-link' href=''>Контакти</a>
+        <a className='menu-btn' href=''>Замовити</a>
+    </div>
+  );
+}
+
+export default Menu;
