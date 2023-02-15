cdbf9d449b92efd68ede5cc33b5a2f8e540b8934
kozakevich13 <hanibalsmitti@gmail.com>
2023-02-15 13:46:53 +0200

    add new component "content"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header';
+import Content from './components/content/Content';
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <Content/>
     </div>
   );
 }
---
+++ src/components/content/Content.jsx
@@ -0,0 +1,20 @@
+import './Content.css';
+import Logo from '../logo/Logo'
+import Menu from '../menu/Menu';
+
+
+function Content() {
+  return (
+    <div className="content">
+      <div className='text-content'>
+        <p className='content-header'>Кава в зернах</p>
+        <p className='main-content'>Кава - це супутник творчих людей.
+Якщо ранок починати з ароматної кави ,то
+день стане куди цікавішим</p>
+      </div>
+     
+    </div>
+  );
+}
+
+export default Content;
