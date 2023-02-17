22467cec20fd205133378224f724e46920bb810b
kozakevich13 <hanibalsmitti@gmail.com>
2023-02-17 17:19:22 +0200

    add new component navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header';
 import Content from './components/content/Content';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        {/* <Navbar/> */}
         <Content/>
     </div>
   );
