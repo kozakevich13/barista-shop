0d47aebcfe661cdbb1cc7bed3808cce7c9082d2a
unknown <hanibalsmitti@gmail.com>
2023-02-20 16:59:25 +0300

    test new nav bar

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Navbar from './components/navbar/Navbar';
 function App() {
   return (
     <div className="App">
-        <Header/>
-        {/* <Navbar/> */}
+        {/* <Header/> */}
+        <Navbar/>
         <Content/>
     </div>
   );
